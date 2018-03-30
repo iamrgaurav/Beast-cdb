64a001ca0168b86092bd5d1639f551b38851477d
Gaurav <graheja1997@gmail.com>
2018-03-30 08:21:43 +0530

    data test

--- src/models/api/tsp/tsp.py
+++ src/models/api/tsp/tsp.py
@@ -15,20 +15,26 @@ class TSPApi:
                 'sim': [{'lsa': sim.lsa,
                          'tsp': sim.tsp,
                          'mobile': sim.sim_no,
-                         'issue_date':sim.issue_date.strftime("%Y-%m-%d")
-                         } for sim in sims if tsp == sim.tsp]if sims is not None else 0,
+                         'issue_date': sim.issue_date.strftime("%Y-%m-%d")
+                         } for sim in sims if tsp == sim.tsp] if sims is not None else 0,
                 'sims_by_other_tsp': Sim.get_sim_count_by_tsp(aadhaar),
-                'Total_Sim':len(sims)
+                'Total_Sim': len(sims)
                 }
-    @staticmethod
 
+    @staticmethod
     def save_sim(mobile, tsp, issue_date, lsa, aadhaar_no):
         user = User.get_by_aadhaar(aadhaar_no)
         if user:
-            return Sim(aadhaar_no,mobile,tsp,lsa,issue_date).save_to_db()
+            return Sim(aadhaar_no, mobile, tsp, lsa, issue_date).save_to_db()
         else:
-            data = requests.post(fau, data={'aadhaar_no':aadhaar_no}).json()
-            if User(aadhaar_no, data['name'],data['dob'],data['address'],data['phone'],data['gender']).save_to_db():
+            data = requests.post(fau, data={'aadhaar_no': aadhaar_no}).json()
+            user_info = {
+                'aadhaar_no': data['aadhaar'],
+                'name': data['name'],
+                'dob': data['dob'],
+                'address': data['address'],
+                'mobile_no': data['phone'],
+                'gender': data['gender']}
+            if User(**user_info).save_to_db():
                 return Sim(aadhaar_no, mobile, tsp, lsa, issue_date).save_to_db()
-
 
