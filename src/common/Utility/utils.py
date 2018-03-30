6aeee2695e6330eb63597a5d0bfc35a0ff7b7a80
Gaurav <graheja1997@gmail.com>
2018-03-30 21:55:05 +0530

    TSP data

--- src/common/Utility/utils.py
+++ src/common/Utility/utils.py
@@ -77,6 +77,7 @@ class Utils:
             "template": "6",
         }
         url = 'http://control.msg91.com/api/sendotp.php'
-        if requests.post(url, payload):
-            return True
+        #if requests.post(url, payload):
+        #    return True
+        return True
 
