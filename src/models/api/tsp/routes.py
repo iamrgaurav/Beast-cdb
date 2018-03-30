772aa4d3f496a98923644576b01cb84cdf43e207
Gaurav <graheja1997@gmail.com>
2018-03-30 10:56:43 +0530

    DAta change

--- src/models/api/tsp/routes.py
+++ src/models/api/tsp/routes.py
@@ -1,42 +1,39 @@
-import datetime
+from flask_restplus import Resource, Namespace
+from flask import jsonify
+from flask import request
 
-import requests
+from src.models.api.tsp.tsp import TSPApi
 
-from src.models.sim.sim import Sim
-from src.config import fake_aadhaar_url as fau
-from src.models.users.user import User
+TSP_namespace = Namespace('TSP','There are Various Operations regarding TSP')
 
 
-class TSPApi:
-    @staticmethod
-    def get_sims_by_aadhaar(aadhaar, tsp):
-        sims = Sim.get_by_aadhaar(aadhaar)
-        return {'aadhaar_no': aadhaar,
-                'sim': [{'lsa': sim.lsa,
-                         'tsp': sim.tsp,
-                         'mobile': sim.sim_no,
-                         'issue_date': sim.issue_date.strftime("%Y-%m-%d")
-                         } for sim in sims if tsp == sim.tsp] if sims is not None else 0,
-                'sims_by_other_tsp': Sim.get_sim_count_by_tsp(aadhaar),
-                'Total_Sim': len(sims)
-                }
+@TSP_namespace.route('/sim')
+class User_Info(Resource):
+    @TSP_namespace.doc(params={
+        'aadhaar_no': {'in': 'formData', 'description': 'User Aadhaar Number', 'required': 'True'},
+        'tsp': {'in': 'formData', 'description': 'TSP Name', 'required': 'True'}})
+    def post(self):
+        aadhaar_no = request.form['aadhaar_no']
+        tsp = request.form['tsp']
+        sims = TSPApi.get_sims_by_aadhaar(aadhaar_no,tsp)
+        return jsonify({"data": sims})
+@TSP_namespace.route('/')
+class User_Info_add(Resource):
+    @TSP_namespace.doc(params={
+        'aadhaar_no': {'in': 'formData', 'description': 'User Aadhaar Number', 'required': 'True'},
+        'mobile_no':{'in': 'formData', 'description': 'User Phone Number in Format +91xxxxxxxxxxx', 'required': 'True'},
+        'tsp':{'in': 'formData', 'description': 'Name of TSP', 'required': 'True'},
+        'issue_date':{'in': 'formData', 'description': 'Issue Date Time', 'required': 'True'},
+        'lsa':{'in': 'formData', 'description': 'LSA', 'required': 'True'},
+    })
+    def post(self):
+        mobile = request.form['mobile_no']
+        tsp = request.form['tsp']
+        issue_date = request.form['issue_date']
+        lsa = request.form['lsa']
+        aadhaar_no = request.form['aadhaar_no']
+        return 200 if TSPApi.save_sim(mobile,tsp,issue_date,lsa,aadhaar_no) else 400
 
-    @staticmethod
-    def save_sim(mobile, tsp, issue_date, lsa, aadhaar_no):
-        user = User.get_by_aadhaar(aadhaar_no)
-        if user:
-            return Sim(aadhaar_no, mobile, tsp, lsa, issue_date).save_to_db()
-        else:
-            data = requests.post(fau, data={'aadhaar_no': aadhaar_no}).json()
-            if data is not None:
-                user_info = {
-                    'aadhaar_no': data['aadhaar'],
-                    'name': data['name'],
-                    'dob': data['dob'],
-                    'address': data['address'],
-                    'mobile_no': data['phone'],
-                    'gender': data['gender']}
-                if User(**user_info).save_to_db():
-                    return Sim(aadhaar_no, mobile, tsp, lsa, issue_date).save_to_db()
-            else:
-                return None
+
+
+
