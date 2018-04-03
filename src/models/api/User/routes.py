c7cd1dba355740fdbaa18860e16dc12efdd51ff1
Gaurav <graheja1997@gmail.com>
2018-04-03 10:17:11 +0530

    data corrected

--- src/models/api/User/routes.py
+++ src/models/api/User/routes.py
@@ -18,12 +18,8 @@ def verify_token(token):
     return False
 
 user_model = user_namespace.model('User', {
-    '': fields.String(required=True, description='User Aadhaar Number'),
-    'name': fields.String(required=True, description='User Name'),
-    'dob': fields.String(required=True, description='User Date of Birth'),
-    'mobile_no': fields.String(required=True, description='User Mobile Number'),
-    'gender': fields.String(required=True, description='User Gender'),
-    'address': fields.String(required=True, description='User Address'),
+    'aadhaar_no': fields.String(required=True, description='User Aadhaar Number'),
+    'mobile_no': fields.String(required=True, description='User Mobile Number')
 })
 
 
@@ -36,12 +32,7 @@ class ListUser(Resource):
     @user_namespace.response(201, 'User Has been created Successfuly', user_model)
     @user_namespace.doc(params={
         'aadhaar_no': {'in': 'formData', 'description': 'User Aadhaar Number', 'required': 'True'},
-        'name': {'in': 'formData', 'description': 'User Name', 'required': 'True'},
-        'dob': {'in': 'formData', 'description': 'User Date of Birth', 'required': 'True'},
-        'address': {'in': 'formData', 'description': 'User Address', 'required': 'True'},
-
-        'mobile_no': {'in': 'formData', 'description': 'User Mobile Number', 'required': 'True'},
-        'gender': {'in': 'formData', 'description': 'User Gender', 'required': 'True'},
+        'mobile_no': {'in': 'formData', 'description': 'User Mobile Number', 'required': 'True'}
     })
     def post(self):
         aadhaar_no = request.form['aadhaar_no']
@@ -50,11 +41,10 @@ class ListUser(Resource):
         address = request.form['address']
         mobile_no = request.form['mobile_no']
         gender = request.form['gender']
-        if UserAPI.create_new_user(aadhaar_no=aadhaar_no, name=name, dob=dob, address=address, mobile_no=mobile_no,
-                                   gender=gender):
-            return 201
+        if UserAPI.create_new_user(aadhaar_no=aadhaar_no, mobile_no=mobile_no):
+            return {"msg":"User Created successfuly"}, 201
         else:
-            return 400
+            return {"msg": "User can't be Created"}, 400
 
 
 @user_namespace.route('/<string:user_id>')
@@ -76,10 +66,9 @@ class SendOTPtoUser(Resource):
 class Authorize(Resource):
     @user_namespace.doc(params={
         'otp': {'in': 'formData', 'description': 'OTP Sent To User', 'required': 'True'}})
-    @user_namespace.response(200, "You are Authorize")
     def post(self, otp_id):
         user_otp = request.form['otp']
-        return jsonify(UserAPI.authenticate_user(otp_id, user_otp))
+        return jsonify(UserAPI.authenticate_user(otp_id, user_otp)), 200
 
 
 @user_namespace.route('/sim-registered/<string:user_id>')
